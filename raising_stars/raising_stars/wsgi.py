5ace13097c20ae79d59efb53766323451955ca03
Daphine Nambafu <daphinenambafu@gmail.com>
2024-04-03 10:07:38 +0300

    raising stars work

---
+++ raising_stars/raising_stars/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for raising_stars project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'raising_stars.settings')
+
+application = get_wsgi_application()
